0a9d0bc679b0f5947a1014e4134bb2e20b4e9419
Sandip Tamang <136832190+YetiTheSleepingChancellor@users.noreply.github.com>
2025-06-06 21:06:28 +0545

    12th commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Portfolio__V.1/',  // This *must* match your GitHub repo name EXACTLY, including case
-  plugins: [react()]
-})
+  plugins: [react()],
+  base: '/Portfolio__V.1/', // Match this exactly with your GitHub repository name
+});
