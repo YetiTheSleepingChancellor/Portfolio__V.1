087bf5d0a222a7f9d93e18a0e4e95a2d724406f6
Sandip Tamang <136832190+YetiTheSleepingChancellor@users.noreply.github.com>
2025-06-06 14:50:14 +0545

    2nd commit

---
+++ src/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Button() {
+    const ButtonStyle = {
+
+    }
+
+    return (
+        <button type={type} onclick={onclick} className="{"></button>
+    )
+}
